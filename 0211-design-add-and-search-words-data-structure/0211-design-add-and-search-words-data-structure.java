665bd265e16edb1d10e528364f9aebead1cc0bdb
Shubham Patidar <ptdrshubham87@gmail.com>
2026-04-25 22:20:24 +0530

    Time: 185 ms (78.53%), Space: 273.9 MB (76.67%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,54 @@
+class WordDictionary {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd = false;
+    }
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode node = root;
+
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+
+            node = node.children[index];
+        }
+
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private boolean dfs(String word, int i, TrieNode node) {
+        if (node == null) return false;
+
+        if (i == word.length()) return node.isEnd;
+
+        char ch = word.charAt(i);
+
+        if (ch == '.') {
+            // try all possibilities
+            for (TrieNode child : node.children) {
+                if (child != null && dfs(word, i + 1, child)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            int index = ch - 'a';
+            return dfs(word, i + 1, node.children[index]);
+        }
+    }
+}
