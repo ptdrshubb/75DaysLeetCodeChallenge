44a534368efed051c7dff8cf8aa3b622656ed186
Shubham Patidar <ptdrshubham87@gmail.com>
2026-05-21 21:33:27 +0530

    Time: 150 ms (72.57%), Space: 46.8 MB (87.47%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -0,0 +1,70 @@
+class Solution {
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word;
+    }
+
+    public List<String> findWords(char[][] board, String[] words) {
+        TrieNode root = buildTrie(words);
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(board, i, j, root, result);
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(char[][] board, int i, int j, TrieNode node, List<String> result) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length) {
+            return;
+        }
+
+        char ch = board[i][j];
+
+        if (ch == '#' || node.children[ch - 'a'] == null) {
+            return;
+        }
+
+        node = node.children[ch - 'a'];
+
+        if (node.word != null) {
+            result.add(node.word);
+            node.word = null;
+        }
+
+        board[i][j] = '#';
+
+        dfs(board, i + 1, j, node, result);
+        dfs(board, i - 1, j, node, result);
+        dfs(board, i, j + 1, node, result);
+        dfs(board, i, j - 1, node, result);
+
+        board[i][j] = ch;
+    }
+
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+
+        for (String word : words) {
+            TrieNode node = root;
+
+            for (char ch : word.toCharArray()) {
+                int index = ch - 'a';
+
+                if (node.children[index] == null) {
+                    node.children[index] = new TrieNode();
+                }
+
+                node = node.children[index];
+            }
+
+            node.word = word;
+        }
+
+        return root;
+        
+    }
+}
